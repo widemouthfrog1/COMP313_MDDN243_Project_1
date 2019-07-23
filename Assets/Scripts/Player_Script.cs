9fe7eeb98fcbd1ad7b2638297715d04801f11625
Karl Bennett <widemouthfrog1@gmail.com>
2019-07-23 17:52:48 +1200

    Add player script shell
    I've added a script with public variables showing where the controls will be inputted (this will make updating the controls easy). I've also added some extra folders to store our different items

---
+++ Assets/Scripts/Player_Script.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Script : MonoBehaviour
+{
+    public string toSquare;
+    public string toCircle;
+    public string toTriangle;
+    public string rotateLeft;
+    public string rotateRight;
+    public string extendPistons;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
