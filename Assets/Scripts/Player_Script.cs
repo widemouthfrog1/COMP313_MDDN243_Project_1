365d08aea85aca2256453aed585180cfa71163f5
Karl Bennett <widemouthfrog1@gmail.com>
2019-07-24 21:39:34 +1200

    Add movement and correct colliders
    Colliders now change to the correct collider for the given shape. You can now move left and right with the circle however there is a bug where angular velocity/angular acceleration (torque) is not reduced to zero upon stopping after changing to the square or triangle. This means that sometimes when you move with the circle, change into a triangle and stop, and change back, you'll start moving again without pressing any buttons.

--- Assets/Scripts/Player_Script.cs
+++ Assets/Scripts/Player_Script.cs
@@ -27,18 +27,19 @@ public class Player_Script : MonoBehaviour
     private ArrayList toCircleControls;
     private ArrayList toTriangleControls;
 
+    private bool rotatingLeft;
     private ArrayList rotateLeftControls;
+    private bool rotatingRight;
     private ArrayList rotateRightControls;
 
+    private bool extended;//true if partially or fully extended
+    private int extendVelocity;//negative: contracting, positive: extending, 0: stationary
     private ArrayList extendPistonsControls;
 
     // Start is called before the first frame update
     void Start()
     {
-        //defaults to square when player is created
-        mode = (int)PLAYER_MODE.SQUARE;
-        
-        initialiseArrayLists();
+        initialiseVariables();
         addAllControls();
     }
 
@@ -53,6 +54,17 @@ public class Player_Script : MonoBehaviour
     {
         handleControls();
         updateSprite();
+        updateColliders();
+
+        Rigidbody2D rigidBody = GetComponent<Rigidbody2D>();
+        if (rotatingLeft)
+        {
+            rigidBody.AddTorque(1);
+        }
+        if (rotatingRight)
+        {
+            rigidBody.AddTorque(-1);
+        }
     }
 
     private void handleControls()
@@ -78,6 +90,32 @@ public class Player_Script : MonoBehaviour
             if (Input.GetKeyDown(control))
             {
                 mode = (int)PLAYER_MODE.TRIANGLE;
+                break;//make sure code isn't called twice for multiple key presses
+            }
+        }
+        foreach (string control in rotateLeftControls)
+        {
+            if (Input.GetKeyDown(control))
+            {
+                rotatingLeft = true;
+                break;//make sure code isn't called twice for multiple key presses
+            }
+            if (Input.GetKeyUp(control))
+            {
+                rotatingLeft = false;
+                break;//make sure code isn't called twice for multiple key presses
+            }
+        }
+        foreach (string control in rotateRightControls)
+        {
+            if (Input.GetKeyDown(control))
+            {
+                rotatingRight = true;
+                break;//make sure code isn't called twice for multiple key presses
+            }
+            if (Input.GetKeyUp(control))
+            {
+                rotatingRight = false;
                 break;//make sure code isn't called twice for multiple key presses
             }
         }
@@ -113,8 +151,50 @@ public class Player_Script : MonoBehaviour
         }
     }
 
-    private void initialiseArrayLists()
+    private void updateColliders()
     {
+        BoxCollider2D boxCollider = GetComponent<BoxCollider2D>();
+        CircleCollider2D circleCollider = GetComponent<CircleCollider2D>();
+        PolygonCollider2D polygonCollider = GetComponent<PolygonCollider2D>();
+        if (mode == (int)PLAYER_MODE.SQUARE)
+        {
+            if (boxCollider.enabled == false)
+            {
+                boxCollider.enabled = true;
+            }
+            circleCollider.enabled = false;
+            polygonCollider.enabled = false;
+        }
+        if (mode == (int)PLAYER_MODE.CIRCLE)
+        {
+            if (circleCollider.enabled == false)
+            {
+                circleCollider.enabled = true;
+            }
+            boxCollider.enabled = false;
+            polygonCollider.enabled = false;
+        }
+        if (mode == (int)PLAYER_MODE.TRIANGLE)
+        {
+            if (polygonCollider.enabled == false)
+            {
+                polygonCollider.enabled = true;
+            }
+            boxCollider.enabled = false;
+            circleCollider.enabled = false;
+        }
+    }
+
+    private void initialiseVariables()
+    {
+        //defaults to square when player is created
+        mode = (int)PLAYER_MODE.SQUARE;
+        rotatingLeft = false;
+        rotatingRight = false;
+        extended = false;
+        extendVelocity = 0;
+
+        //initialse array lists
         toSquareControls = new ArrayList();
         toCircleControls = new ArrayList();
         toTriangleControls = new ArrayList();
